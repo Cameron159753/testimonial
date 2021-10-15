69f7da2a6bd11436219aea821ea0d7c02813c1e0
Cameron159753 <60602354+Cameron159753@users.noreply.github.com>
2021-10-15 16:17:42 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+// page 1 and page 2
 function show(shown, hidden) {
   document.getElementById(shown).style.display='block';
   document.getElementById(hidden).style.display='none';
   return false;
 }
+// end
+// navbar
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -11,3 +14,8 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+// end
+
+
+
+
